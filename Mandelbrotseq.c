003acebe4e8ad9601b2acea25cebb13fd888b394
Rami Aabed <rami.aabed@hotmail.com>
2025-11-24 21:05:08 +0200

    Add files via upload

---
+++ Mandelbrotseq.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <time.h>
+
+#define WIDTH 640
+#define HEIGHT 480
+#define MAX_ITER 255
+
+struct complex {
+    double real;
+    double imag;
+};
+
+int cal_pixel(struct complex c) {
+    double z_real = 0;
+    double z_imag = 0;
+    double z_real2, z_imag2, lengthsq;
+    int iter = 0;
+    do {
+        z_real2 = z_real * z_real;
+        z_imag2 = z_imag * z_imag;
+        z_imag = 2 * z_real * z_imag + c.imag;
+        z_real = z_real2 - z_imag2 + c.real;
+        lengthsq = z_real * z_real + z_imag * z_imag;  // Fixed: use new z values
+        iter++;
+    } while ((iter < MAX_ITER) && (lengthsq < 4.0));
+    return iter;
+}
+
+void save_pgm(const char *filename, int image[HEIGHT][WIDTH]) {
+    FILE* pgmimg;
+    pgmimg = fopen(filename, "wb");
+    fprintf(pgmimg, "P2\n");
+    fprintf(pgmimg, "%d %d\n", WIDTH, HEIGHT);
+    fprintf(pgmimg, "255\n");
+    for (int i = 0; i < HEIGHT; i++) {
+        for (int j = 0; j < WIDTH; j++) {
+            fprintf(pgmimg, "%d ", image[i][j]);
+        }
+        fprintf(pgmimg, "\n");
+    }
+    fclose(pgmimg);
+}
+
+int main() {
+    int image[HEIGHT][WIDTH];
+    struct complex c;
+    clock_t start_time = clock();
+    for (int i = 0; i < HEIGHT; i++) {
+        for (int j = 0; j < WIDTH; j++) {
+            c.real = (j - WIDTH / 2.0) * 4.0 / WIDTH;
+            c.imag = (i - HEIGHT / 2.0) * 4.0 / HEIGHT;
+            image[i][j] = cal_pixel(c);
+        }
+    }
+    clock_t end_time = clock();
+    double total_time = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
+    printf("Execution time: %f seconds\n", total_time);
+    save_pgm("mandelbrot_seq.pgm", image);
+    return 0;
+}
